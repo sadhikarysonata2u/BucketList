c617358a5730d5881c1c7f4ccf785576205c67a3
Sujoy Adhikary <Sujoy.Adhikary@sonata-software.com>
2025-09-05 20:00:03 +0530

    Initial Commit

---
+++ BucketListTests/BucketListTests.swift
@@ -0,0 +1,16 @@
+//
+//  BucketListTests.swift
+//  BucketListTests
+//
+//  Created by SUJOY on 05/09/25.
+//
+
+import Testing
+
+struct BucketListTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
